5da916af2b1768e66bcbabfa253bd5671fea09e7
jjcruzs <JJCRUZ@ESTUDIANTE.UNIAJC.EDU.CO>
2025-02-28 19:33:30 -0500

    Persona.java

--- Ejercicio_Clase/src/ejercicio_clase/Persona.java
+++ Ejercicio_Clase/src/ejercicio_clase/Persona.java
@@ -10,16 +10,26 @@ public class Persona {
     public String apellido;
     public int identificacion;
     public String fechaNacimiento;
+    public String paisNacimiento;
+    public char genero;
 
-    public Persona(String nombre, String apellido, int identificacion, String fechaNacimiento) {
+    public Persona(String nombre, String apellido, int identificacion, String fechaNacimiento, String paisNacimiento, char genero) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.identificacion = identificacion;
         this.fechaNacimiento = fechaNacimiento;
+        this.paisNacimiento = paisNacimiento;
+        
+       
+        if (genero == 'H' || genero == 'M') {
+            this.genero = genero;
+        } else {
+            System.out.println("Género inválido. Se asignará 'H' por defecto.");
+            this.genero = 'H';
+        }
     }
     
-    
-    public int calcularEdad(){
+    public int calcularEdad() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         LocalDate fechaNac = LocalDate.parse(fechaNacimiento, formatter);
         LocalDate hoy = LocalDate.now();
@@ -27,12 +37,14 @@ public class Persona {
         return periodo.getYears();
     }
     
-    
-    public void imprimir(){
+    public void imprimir() {
         System.out.println("Nombre: " + nombre);
         System.out.println("Apellido: " + apellido);
-        System.out.println("Identificacion: " + identificacion);
-        System.out.println("Fecha nacimiento: " + fechaNacimiento);
+        System.out.println("Identificación: " + identificacion);
+        System.out.println("Fecha de nacimiento: " + fechaNacimiento);
+        System.out.println("País de nacimiento: " + paisNacimiento);
+        System.out.println("Género: " + (genero == 'H' ? "Hombre" : "Mujer"));
         System.out.println("Edad: " + calcularEdad() + " años");
     }
 }
+
