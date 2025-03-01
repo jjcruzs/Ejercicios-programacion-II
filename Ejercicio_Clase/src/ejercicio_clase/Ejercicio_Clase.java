30662750912446d3a816e81827d3b524492d20a9
jjcruzs <JJCRUZ@ESTUDIANTE.UNIAJC.EDU.CO>
2025-02-28 19:32:56 -0500

    Ejercicio_Clase.java

--- Ejercicio_Clase/src/ejercicio_clase/Ejercicio_Clase.java
+++ Ejercicio_Clase/src/ejercicio_clase/Ejercicio_Clase.java
@@ -4,17 +4,19 @@ public class Ejercicio_Clase {
 
     public static void main(String[] args) {
         
-        Persona persona1 = new Persona("Juan", "Cruz", 4444, "14/02/2000");
-   
-        Persona persona2 = new Persona("Jose", "Serna", 4444, "15/06/2070");
+        Persona persona1 = new Persona("Juan", "Cruz", 4444, "14/02/2000", "Colombia", 'H');
+        Persona persona2 = new Persona("Jose", "Serna", 4444, "15/06/1995", "México", 'H');
+        Persona persona3 = new Persona("Maria", "Lopez", 5555, "20/08/1998", "Argentina", 'M');
         
-        System.out.println("Informacion persona 1: ");
+        System.out.println("Información persona 1: ");
         persona1.imprimir();
         
-        System.out.println("\nInformacion persona 2: ");
+        System.out.println("\nInformación persona 2: ");
         persona2.imprimir();
         
-        }
-
+        System.out.println("\nInformación persona 3: ");
+        persona3.imprimir();
     }
+}
+
 
